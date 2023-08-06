985d3e2020ed5b0c6e904905de169ccd7137b0ad
Anilson Lopes <nissobmx@gmail.com>
2023-08-06 12:08:34 -0300

    :construction: update index

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex min-h-screen flex-col bg-gray-100 dark:bg-gray-700">
     <la-header />
-    <div class="mx-auto max-w-2xl px-6 lg:px-8">
+    <div class="mx-auto max-w-6xl px-6 lg:px-8">
       <slot />
     </div>
   </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,18 @@
 <template>
-  <div class="flex flex-col min-h-screen justify-center">
-    <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-      <div
-        class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:text-gray-300 dark:ring-gray-100/10 dark:hover:ring-gray-100/20"
-      >
-        Como ser um programador melhor
-        <nuxt-link
-          to="/como-ser-um-programador-melhor"
-          class="ml-3 font-semibold text-blue-600 dark:text-blue-400"
-        >
-          Saiba mais <span aria-hidden="true">&rarr;</span>
-        </nuxt-link>
-      </div>
-    </div>
-    <div class="text-center">
-      <h1
-        class="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-6xl"
-      >
-        Acessível, escalável, autossustentável.
-      </h1>
-      <p class="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300">
-        Gosto de ter em mente esses três itens no desenvolvimento de qualquer
-        coisa. É comum ter dificuldade em manter esses três pilares alinhado,
-        mas mantendo a intensão, tudo estará bem.
-      </p>
-      <div class="mt-10 flex items-center justify-center gap-x-6">
-        <nuxt-link
-          to="/como-ser-um-programador-melhor"
-          class="rounded-md bg-red-600 px-5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-700"
-        >
-          Inicie agora
-        </nuxt-link>
-        <nuxt-link
-          to="/posts"
-          class="text-sm font-semibold leading-6 text-gray-900 dark:text-gray-100"
-        >
-          Meus posts <span aria-hidden="true">→</span>
-        </nuxt-link>
-      </div>
-    </div>
+  <div class="min-h-screen xl:grid xl:grid-cols-2 xl:gap-5">
+    <h1
+      class="text-4xl font-bold tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl lg:mt-20 lg:text-6xl"
+    >
+      Acessível, escalável, autossustentável.
+    </h1>
+    <p
+      class="mt-5 text-lg leading-8 text-gray-600 dark:text-gray-300 lg:text-2xl lg:font-thin xl:mt-20"
+    >
+      É comum terem dificuldade em manter esses três pilares alinhado, mas
+      mantendo a intensão, tudo estará bem.
+    </p>
+    <div
+      class="mt-10 h-48 w-full rounded-2xl bg-zinc-200 xl:col-span-2 xl:h-96"
+    ></div>
   </div>
 </template>
