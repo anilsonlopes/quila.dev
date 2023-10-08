b8b6cf4a63e1fcc788d1e03c53dd9ee3bb0ff43c
Anilson Lopes <nissobmx@gmail.com>
2023-10-08 12:27:14 -0300

    add openai

--- plugins/gpt.ts
+++ plugins/gpt.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
-  dangerouslyAllowBrowser: true,
+  dangerouslyAllowBrowser: false,
 });
 
 export default defineNuxtPlugin(async (nuxtApp) => {
