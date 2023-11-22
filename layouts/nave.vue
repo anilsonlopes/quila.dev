5d5b23d22bb8420048bb2bbde4f59ae721f05db7
Anilson Lopes <nissobmx@gmail.com>
2023-11-22 20:14:03 -0300

    Add nave layout and nave index page

---
+++ layouts/nave.vue
@@ -0,0 +1,6 @@
+
+<template>
+  <div class="h-screen bg-blue-100">
+    <slot />
+  </div>
+</template>
---
+++ pages/nave/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="p-10">
+    <div class="mb-2 text-xl font-bold text-gray-900">Atividade</div>
+    <ul class="space-y-4">
+      <li v-for="index in 7">
+        <div class="rounded bg-white p-3">
+          <div class="font-semibold text-gray-500">2134</div>
+          <div class="font-medium text-gray-700 uppercase">Marechal</div>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  layout: "nave",
+});
+</script>
