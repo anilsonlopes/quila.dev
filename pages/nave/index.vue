529c7bf4092c530fb8b974ce335800705a537128
Anilson Lopes <nissobmx@gmail.com>
2023-11-22 21:36:44 -0300

    Refactor nave/index.vue component

--- pages/nave/index.vue
+++ pages/nave/index.vue
@@ -1,12 +1,32 @@
 <template>
-  <div class="p-10">
-    <div class="mb-2 text-xl font-bold text-gray-900">Atividade</div>
-    <ul class="space-y-4">
-      <li v-for="index in 7">
-        <div class="rounded bg-white p-3">
-          <div class="font-semibold text-gray-500">2134</div>
-          <div class="font-medium text-gray-700 uppercase">Marechal</div>
+  <div class="p-5 flex flex-col items-center">
+    <h1 class=" font-racing text-5xl">Vai de nave</h1>
+    <img src="/flame-searching.gif" alt="Nave" />
+    <p class="text-gray-800 text-lg mt-8">Bem-vindo à revolução do transporte alternativo. Vai de nave é o passaporte para uma jornada tranquila, segura e conveniente.</p>
+    <p class="text-gray-800 text-lg">Conectamos motoristas de van comprometidos com a excelência ao redor do universo, prontos para levá-lo aos destinos mais incríveis.</p>
+    <p class="text-xl mt-5">Porque ir de nave?</p>
+    <ul class="space-y-8">
+      <li class="bg-gray-100 p-8">
+        <div class="text-3xl">✨</div>
+        <div class="text-gray-700 mt-2"><strong>Viagens Espaciais Confortáveis:</strong></div> 
+        <div class="mt-2 text-gray-600">
+          Embarque em nossas naves espaciais modernas, projetadas para oferecer o máximo conforto durante toda a jornada.
         </div>
+      </li>
+      <li class="bg-gray-100 p-8">
+        🛣️ <strong>Rotas Intermunicipais Eficientes:</strong> Navegue por rotas intermunicipais otimizadas, economizando tempo e garantindo chegadas pontuais.
+      </li>
+      <li class="bg-gray-100 p-8">
+        🤝 <strong>Motoristas de Confiança:</strong> Nossa tripulação de motoristas é cuidadosamente selecionada, garantindo profissionalismo, segurança e um serviço excepcional.
+      </li>
+      <li class="bg-gray-100 p-8">
+        🌐 <strong>Conexão Direta:</strong> Reduza a distância entre destinos com a facilidade de uma conexão direta entre motoristas de van qualificados e passageiros.
+      </li>
+      <li class="bg-gray-100 p-8">
+        📱 <strong>Reservas Simples:</strong> Faça suas reservas com facilidade através do nosso aplicativo intuitivo. A sua jornada começa com apenas alguns toques na tela.
+      </li>
+      <li class="bg-gray-100 p-8">
+        <strong>Pronto para decolar?</strong> Deixe a Vai de Nave ser o guia da sua próxima aventura intermunicipal. Junte-se a nós e experimente uma forma revolucionária de viajar. Seu destino está a apenas um clique de distância! 🌌✨
       </li>
     </ul>
   </div>
