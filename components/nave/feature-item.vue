4ddc92dfdc867b9adfefa5655035fcfa7eaa391b
Anilson Lopes <nissobmx@gmail.com>
2023-11-23 11:06:35 -0300

    Refactor navigation page layout and content

---
+++ components/nave/feature-item.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+const props = defineProps({
+  icon: String,
+  title: String,
+  body: String,
+});
+</script>
+
+<template>
+  <div class="bg-gray-100 p-8 rounded-lg shadow border border-gray-300">
+    <div class="text-5xl">{{ props.icon }}</div>
+    <div class="text-gray-700 text-lg mt-5">
+      <strong>{{ props.title }}</strong>
+    </div> 
+    <div class="mt-2 text-gray-600">
+      {{ props.body }}
+    </div>
+  </div>
+</template>
--- pages/nave/index.vue
+++ pages/nave/index.vue
@@ -1,32 +1,20 @@
 <template>
-  <div class="p-5 flex flex-col items-center">
-    <h1 class=" font-racing text-5xl">Vai de nave</h1>
-    <img src="/flame-searching.gif" alt="Nave" />
-    <p class="text-gray-800 text-lg mt-8">Bem-vindo à revolução do transporte alternativo. Vai de nave é o passaporte para uma jornada tranquila, segura e conveniente.</p>
-    <p class="text-gray-800 text-lg">Conectamos motoristas de van comprometidos com a excelência ao redor do universo, prontos para levá-lo aos destinos mais incríveis.</p>
-    <p class="text-xl mt-5">Porque ir de nave?</p>
+  <div class="p-5 flex flex-col">
+    <h1 class="font-racing text-5xl mt-4 text-center">Vai de nave</h1>
+    <p class="text-gray-700 text-lg text-center mt-2">Bem-vindo à revolução do transporte alternativo.</p>
+    <img src="/flame-searching.gif" alt="Nave" class="mt-5" />
+    <ul class="pl-4 mt-5 list-disc space-y-5 text-gray-700">
+      <li>
+        Vai de nave é o passaporte para uma jornada tranquila, segura e conveniente.
+      </li>
+      <li>
+        Conectamos motoristas de van comprometidos com a excelência ao redor do universo, prontos para levá-lo aos destinos mais incríveis.
+      </li>
+    </ul>
+    <h2 class="text-xl text-gray-800 mt-10 mb-3 font-semibold">Porque ir de nave?</h2>
     <ul class="space-y-8">
-      <li class="bg-gray-100 p-8">
-        <div class="text-3xl">✨</div>
-        <div class="text-gray-700 mt-2"><strong>Viagens Espaciais Confortáveis:</strong></div> 
-        <div class="mt-2 text-gray-600">
-          Embarque em nossas naves espaciais modernas, projetadas para oferecer o máximo conforto durante toda a jornada.
-        </div>
-      </li>
-      <li class="bg-gray-100 p-8">
-        🛣️ <strong>Rotas Intermunicipais Eficientes:</strong> Navegue por rotas intermunicipais otimizadas, economizando tempo e garantindo chegadas pontuais.
-      </li>
-      <li class="bg-gray-100 p-8">
-        🤝 <strong>Motoristas de Confiança:</strong> Nossa tripulação de motoristas é cuidadosamente selecionada, garantindo profissionalismo, segurança e um serviço excepcional.
-      </li>
-      <li class="bg-gray-100 p-8">
-        🌐 <strong>Conexão Direta:</strong> Reduza a distância entre destinos com a facilidade de uma conexão direta entre motoristas de van qualificados e passageiros.
-      </li>
-      <li class="bg-gray-100 p-8">
-        📱 <strong>Reservas Simples:</strong> Faça suas reservas com facilidade através do nosso aplicativo intuitivo. A sua jornada começa com apenas alguns toques na tela.
-      </li>
-      <li class="bg-gray-100 p-8">
-        <strong>Pronto para decolar?</strong> Deixe a Vai de Nave ser o guia da sua próxima aventura intermunicipal. Junte-se a nós e experimente uma forma revolucionária de viajar. Seu destino está a apenas um clique de distância! 🌌✨
+      <li v-for="feature in features">
+        <NaveFeatureItem v-bind="feature" />
       </li>
     </ul>
   </div>
@@ -36,4 +24,37 @@
 definePageMeta({
   layout: "nave",
 });
+
+const features = [
+  {
+    icon: "✨",
+    title: "Viagens confortáveis:",
+    body: "Embarque em nossas naves espaciais modernas, projetadas para oferecer o máximo conforto durante toda a jornada.",
+  },
+  {
+    icon: "🛣️",
+    title: "Rotas intermunicipais eficientes:",
+    body: "Navegue por rotas intermunicipais otimizadas, economizando tempo e garantindo chegadas pontuais.",
+  },
+  {
+    icon: "🤝",
+    title: "Motoristas de confiança:",
+    body: "Nossa tripulação de motoristas é cuidadosamente selecionada, garantindo profissionalismo, segurança e um serviço excepcional.",
+  },
+  {
+    icon: "🚌",
+    title: "Conexão direta:",
+    body: "Reduza a distância entre destinos com a facilidade de uma conexão direta entre motoristas de van qualificados e passageiros.",
+  },
+  {
+    icon: "🍃",
+    title: "Reservas simples:",
+    body: "Faça suas reservas com facilidade através do nosso aplicativo intuitivo. A sua jornada começa com apenas alguns toques na tela.",
+  },
+  {
+    icon: "🛸",
+    title: "Pronto para decolar?",
+    body: "Deixe a Vai de Nave ser o guia da sua próxima aventura intermunicipal. Junte-se a nós e experimente uma forma revolucionária de viajar. Seu destino está a apenas um clique de distância!",
+  }
+]
 </script>
