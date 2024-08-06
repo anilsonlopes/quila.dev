be2f11e1ec2527f7811d5e3cd73d0069b6164d0b
Anilson Lopes <nissobmx@gmail.com>
2024-08-06 20:15:19 -0300

    Update landing page layout and styles

--- pages/index.vue
+++ pages/index.vue
@@ -25,13 +25,16 @@ const quoteGenerate = (post: ParsedContent) => {
 
   return firstChildOfChild.value;
 };
+
+const postPlaceholder =
+  "https://images.unsplash.com/photo-1573588028698-f4759befb09a?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 </script>
 
 <template>
   <div class="grid grid-cols-12 gap-6 lg:gap-12">
     <div class="col-span-12 grid grid-cols-12 gap-4 lg:col-span-8 lg:gap-10">
       <div class="col-span-12 lg:col-span-6 lg:pr-10">
-        <h1 class="text-3xl font-light text-white lg:text-7xl">
+        <h1 class="text-3xl font-thin text-white lg:text-7xl">
           Cúrriculo disfarçado de portfólio para dev web sem diploma.
         </h1>
         <p class="mt-10 break-words text-sm italic text-gray-300">
@@ -41,11 +44,20 @@ const quoteGenerate = (post: ParsedContent) => {
           ui-ux e beatmaker.
         </p>
         <ul
-          class="mt-10 flex items-center space-x-5 divide-x text-sm font-black italic"
+          class="mt-10 flex items-center space-x-10 divide-x divide-dashed divide-slate-700 text-sm font-black"
         >
-          <li>Maceió, BRA</li>
-          <li class="pl-5">Agosto 21, 1992</li>
-          <li class="pl-5">nissobmx@gmail.com</li>
+          <li>
+            <div class="font-light">Maceió</div>
+            <strong class="block font-bold">BRA</strong>
+          </li>
+          <li class="pl-5">
+            <div class="font-light">Agosto 21</div>
+            <strong class="block font-bold">1992</strong>
+          </li>
+          <li class="pl-5">
+            <div class="font-light">info@quila.dev</div>
+            <strong class="block font-bold">82 993 220 374</strong>
+          </li>
         </ul>
       </div>
       <img
@@ -55,25 +67,26 @@ const quoteGenerate = (post: ParsedContent) => {
       />
     </div>
     <div class="col-span-12 lg:col-span-4">
-      <ul class="space-y-5">
+      <ul class="grid h-full grid-rows-4 gap-5">
         <li v-for="post in posts" :key="post._id">
-          <nuxt-link :to="post._path" class="flex items-center space-x-4">
-            <div class="flex-none">
+          <nuxt-link :to="post._path" class="block">
+            <div class="flex items-center space-x-5">
               <img
-                src="https://images.unsplash.com/photo-1573588028698-f4759befb09a?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                class="h-[130px] w-[130px] object-cover"
-                alt="Imagem"
+                class="h-[150px] w-[150px] flex-none object-cover"
+                :src="post.image || postPlaceholder"
               />
-            </div>
-            <div class="space-y-4">
-              <div class="text-xs">Como ser um programador melhor</div>
-              <div class="line-clamp-2 text-xl">"{{ quoteGenerate(post) }}</div>
-              <div class="flex space-x-3 text-xs">
-                <div>
-                  {{ post.title }}
+              <div class="col-span-2 space-y-4">
+                <div class="text-xs">Como ser um programador melhor</div>
+                <div class="line-clamp-2 text-xl">
+                  "{{ quoteGenerate(post) }}
                 </div>
-                <div>|</div>
-                <div>{{ post.body?.children.length }}min de leitura</div>
+                <div class="flex space-x-3 text-xs">
+                  <div>
+                    {{ post.title }}
+                  </div>
+                  <div>|</div>
+                  <div>{{ post.body?.children.length }}min de leitura</div>
+                </div>
               </div>
             </div>
           </nuxt-link>
