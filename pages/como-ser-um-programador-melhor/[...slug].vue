53b5a5bcbafc88159591a895a406635f8261b3b3
Anilson Lopes <nissobmx@gmail.com>
2024-08-16 17:29:31 -0300

    feat: Add video player and update content layout for post pages

--- pages/como-ser-um-programador-melhor/[...slug].vue
+++ pages/como-ser-um-programador-melhor/[...slug].vue
@@ -10,11 +10,16 @@ const path = `como-ser-um-programador-melhor/${slug}`;
 
 <template>
   <div class="container mx-auto">
-    <div class="mb-10">
-      <la-back fallback="/como-ser-um-programador-melhor" />
-    </div>
-    <div class="prose prose-blue prose-invert pb-10">
-      <content-doc :path="path" />
+    <div class="mt-10 grid grid-cols-2 gap-10">
+      <video
+        poster="https://images.unsplash.com/photo-1615627121117-e3278bc8b1db?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        class="aspect-video w-full rounded object-cover"
+        src="/vidz/sample-video.mp4"
+        controls
+      />
+      <div class="prose prose-blue prose-invert pb-10">
+        <content-doc :path="path" />
+      </div>
     </div>
   </div>
 </template>
