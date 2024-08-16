b0c444388cd5736a542473aa99524abc126d8217
Anilson Lopes <nissobmx@gmail.com>
2024-08-16 17:38:21 -0300

    Update video player and content layout for post pages

--- pages/como-ser-um-programador-melhor/[...slug].vue
+++ pages/como-ser-um-programador-melhor/[...slug].vue
@@ -13,7 +13,7 @@ const path = `como-ser-um-programador-melhor/${slug}`;
     <div class="mt-10 grid grid-cols-2 gap-10">
       <video
         poster="https://images.unsplash.com/photo-1615627121117-e3278bc8b1db?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        class="aspect-video w-full rounded object-cover"
+        class="sticky top-0 aspect-video w-full rounded object-cover"
         src="/vidz/sample-video.mp4"
         controls
       />
--- pages/como-ser-um-programador-melhor/index.vue
+++ pages/como-ser-um-programador-melhor/index.vue
@@ -10,8 +10,16 @@ defineOgImage({
 
 <template>
   <div class="container mx-auto">
-    <div class="prose prose-blue prose-invert pb-10">
-      <content-doc path="/como-ser-um-programador-melhor" />
+    <div class="grid grid-cols-2 gap-10">
+      <video
+        poster="https://images.unsplash.com/photo-1615627121117-e3278bc8b1db?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        class="sticky top-0 aspect-video w-full rounded object-cover"
+        src="/vidz/sample-video.mp4"
+        controls
+      />
+      <div class="prose prose-blue prose-invert pb-10">
+        <content-doc path="/como-ser-um-programador-melhor" />
+      </div>
     </div>
   </div>
 </template>
