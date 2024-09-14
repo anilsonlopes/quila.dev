68dcd3978a532d51b23c4390c14de9c22febaea6
Anilson Lopes <nissobmx@gmail.com>
2024-09-14 20:56:01 -0300

    Refactor footer component and add papapa button

--- components/la-footer.vue
+++ components/la-footer.vue
@@ -1,16 +1,14 @@
 <template>
   <footer class="py-20">
     <div class="container mx-auto flex justify-between">
-      <div class="text-xl font-light">Anilson Lopes</div>
       <ul class="flex gap-6">
         <li>
-          <a
-            href="#"
-            rel="noreferrer"
+          <nuxt-link
+            to="https://linkedin.com/in/anilsonlopes"
             target="_blank"
             class="text-gray-300 transition hover:opacity-75"
           >
-            <span class="sr-only">Facebook</span>
+            <span class="sr-only">LinkedIn</span>
 
             <svg
               class="h-6 w-6"
@@ -18,19 +16,27 @@
               viewBox="0 0 24 24"
               aria-hidden="true"
             >
-              <path
-                fill-rule="evenodd"
-                d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                clip-rule="evenodd"
-              />
+              <g
+                fill="none"
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="1.5"
+              >
+                <path
+                  d="M8 16.375V10.75m4 5.625V13.5m0 0v-2.75m0 2.75c0-1.288 1.222-2 2.4-2c1.6 0 1.6 1.375 1.6 2.875v2m-8-8.75v.5"
+                />
+                <path
+                  d="M3 9.4c0-2.24 0-3.36.436-4.216a4 4 0 0 1 1.748-1.748C6.04 3 7.16 3 9.4 3h5.2c2.24 0 3.36 0 4.216.436a4 4 0 0 1 1.748 1.748C21 6.04 21 7.16 21 9.4v5.2c0 2.24 0 3.36-.436 4.216a4 4 0 0 1-1.748 1.748C17.96 21 16.84 21 14.6 21H9.4c-2.24 0-3.36 0-4.216-.436a4 4 0 0 1-1.748-1.748C3 17.96 3 16.84 3 14.6z"
+                />
+              </g>
             </svg>
-          </a>
+          </nuxt-link>
         </li>
 
         <li>
-          <a
-            href="#"
-            rel="noreferrer"
+          <nuxt-link
+            to="https://instagram.com/quilamcz"
             target="_blank"
             class="text-gray-300 transition hover:opacity-75"
           >
@@ -48,35 +54,12 @@
                 clip-rule="evenodd"
               />
             </svg>
-          </a>
+          </nuxt-link>
         </li>
 
         <li>
-          <a
-            href="#"
-            rel="noreferrer"
-            target="_blank"
-            class="text-gray-300 transition hover:opacity-75"
-          >
-            <span class="sr-only">Twitter</span>
-
-            <svg
-              class="h-6 w-6"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-            >
-              <path
-                d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
-              />
-            </svg>
-          </a>
-        </li>
-
-        <li>
-          <a
-            href="#"
-            rel="noreferrer"
+          <nuxt-link
+            to="https://github.com/anilsonlopes"
             target="_blank"
             class="text-gray-300 transition hover:opacity-75"
           >
@@ -94,13 +77,12 @@
                 clip-rule="evenodd"
               />
             </svg>
-          </a>
+          </nuxt-link>
         </li>
 
         <li>
-          <a
-            href="#"
-            rel="noreferrer"
+          <nuxt-link
+            to="https://dribbble.com/quila"
             target="_blank"
             class="text-gray-300 transition hover:opacity-75"
           >
@@ -118,16 +100,10 @@
                 clip-rule="evenodd"
               />
             </svg>
-          </a>
+          </nuxt-link>
         </li>
       </ul>
-    </div>
-    <div class="mt-10 border-t border-gray-500 pt-10 text-center">
-      <button class="text-gray-600" @click="incrementPapapa">
-        é {{ countPapapa }}
-        <br />
-        papapa
-      </button>
+      <la-papapa />
     </div>
   </footer>
 </template>
