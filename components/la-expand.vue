f4299579632ca1e28b1c7ed56756ec38b925cbe9
Anilson Lopes <nissobmx@gmail.com>
2025-01-12 23:45:08 -0300

    Add la-expand component for expandable content functionality

---
+++ components/la-expand.vue
@@ -0,0 +1,21 @@
+<template>
+  <span
+    role="button"
+    class="bg-black p-2 font-mono text-sm text-green-500"
+    @click="toggle"
+  >
+    {{ expanded ? "" : "<>" }}
+    <span v-show="expanded">
+      <slot />
+    </span>
+  </span>
+</template>
+
+<script lang="ts" setup>
+const expanded = ref(false);
+
+const toggle = () => {
+  //   Track it
+  expanded.value = !expanded.value;
+};
+</script>
