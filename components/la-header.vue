cfe16c09db52f82de5d4319d0ae447b51548ce1a
Anilson Lopes <nissobmx@gmail.com>
2025-01-12 22:50:48 -0300

    update layout and navbar

--- components/la-header.vue
+++ components/la-header.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { onClickOutside } from "@vueuse/core";
 import { useDark, useToggle } from "@vueuse/core";
 
 const route = useRoute();
@@ -13,94 +12,35 @@ const navigation = [
     href: "/",
   },
   {
-    name: "Como ser um programador melhor",
-    href: "/como-ser-um-programador-melhor",
+    name: "Trabalho",
+    href: "/trabalho",
   },
+  { name: "Projetos", href: "/projetos" },
   { name: "Posts", href: "/posts" },
 ];
-
-const mobileMenuOpen = ref(false);
-const mobileMenuElement = ref(null);
-
-onClickOutside(mobileMenuElement, () => {
-  mobileMenuOpen.value = false;
-  enablePageOverflow();
-});
-
-const enablePageOverflow = () => {
-  document.body.style.overflow = "auto";
-};
-
-const disablePageOverflow = () => {
-  document.body.style.overflow = "hidden";
-};
-
-watch(mobileMenuOpen, (isOpen) => {
-  if (isOpen) {
-    disablePageOverflow();
-  } else {
-    enablePageOverflow();
-  }
-});
-
-watch(
-  () => route.fullPath,
-  () => {
-    enablePageOverflow();
-    mobileMenuOpen.value = false;
-  },
-);
 </script>
 
 <template>
-  <header>
-    <nav
-      class="container mx-auto border-b border-neutral-300 py-5 dark:border-neutral-800"
-      aria-label="Global"
-    >
-      <div class="flex w-full justify-between">
-        <button type="button" @click="toggleDark(!isDark)">
-          <Icon
-            :name="isDark ? 'solar:sun-2-linear' : 'solar:moon-linear'"
-            class="text-2xl"
-          />
-        </button>
-        <nuxt-link to="/">
-          <la-quila />
-        </nuxt-link>
-        <button type="button" @click="mobileMenuOpen = !mobileMenuOpen">
-          <Icon name="solar:hamburger-menu-linear" class="text-2xl" />
-        </button>
-      </div>
-      <div v-show="mobileMenuOpen">
-        <div
-          class="fixed inset-0 z-50 overflow-y-auto bg-neutral-100 p-5 dark:bg-neutral-900"
-        >
-          <ul class="space-y-5">
-            <li
-              class="flex items-center justify-between text-sm uppercase dark:text-neutral-400"
-            >
-              <div>Navegação</div>
-              <button type="button" @click="mobileMenuOpen = !mobileMenuOpen">
-                <Icon
-                  name="vaadin:close"
-                  class="text-2xl dark:text-neutral-100"
-                />
-              </button>
-            </li>
-            <li v-for="link in navigation" :key="link.href">
-              <nuxt-link
-                :to="link.href"
-                class="link border-b border-transparent transition-colors hover:border-red-600 dark:text-neutral-200"
-              >
-                <span>
-                  {{ link.name }}
-                </span>
-              </nuxt-link>
-            </li>
-          </ul>
-        </div>
-      </div>
+  <header class="bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-200">
+    <nav class="flex items-center justify-between" aria-label="Global">
+      <ul class="flex space-x-2">
+        <li v-for="link in navigation" :key="link.href">
+          <nuxt-link
+            :to="link.href"
+            class="link block border-b border-transparent px-4 py-2 transition-colors"
+          >
+            <span>
+              {{ link.name }}
+            </span>
+          </nuxt-link>
+        </li>
+      </ul>
+      <button type="button" class="px-3" @click="toggleDark(!isDark)">
+        <Icon
+          :name="isDark ? 'solar:sun-2-linear' : 'solar:moon-linear'"
+          class="text-2xl"
+        />
+      </button>
     </nav>
   </header>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,13 @@
 <template>
   <div
-    class="flex min-h-screen flex-col bg-neutral-100 text-neutral-900 dark:bg-black dark:text-neutral-100"
+    class="flex min-h-screen flex-col bg-neutral-100 text-neutral-900 dark:bg-neutral-900 dark:text-neutral-100"
   >
-    <la-header />
-    <div>
-      <slot />
+    <div class="mx-auto w-full max-w-3xl py-8">
+      <la-header />
+      <div>
+        <slot />
+      </div>
+      <la-footer />
     </div>
-    <la-footer />
   </div>
 </template>
